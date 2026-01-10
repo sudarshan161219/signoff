ac1d7a36815bbba670200cbdd69f0746446b7d4d
sudarshan161219 <godhustler90956@gmail.com>
2026-01-11 03:24:44 +0530

    refactored api mismatch env

--- src/lib/socket/socket.ts
+++ src/lib/socket/socket.ts
@@ -4,7 +4,7 @@ let socket: Socket | null = null;
 
 export const getSocket = () => {
   if (!socket) {
-    const URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+    const URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
     socket = io(URL, {
       withCredentials: true,
       autoConnect: false,
